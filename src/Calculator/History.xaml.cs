988a9d775ded24450772295eeb1b0d8355381319
Saikrishna Ch <saikrishnach2022@gmail.com>
2022-11-17 17:24:20 -0600

    Fixed the New data

--- src/Calculator/History.xaml.cs
+++ src/Calculator/History.xaml.cs
@@ -1,16 +1,39 @@
+using Microsoft.Maui.Controls;
+using System.Collections.ObjectModel;
+
 namespace Calculator;
 
+//[QueryProperty(nameof(HistoryItems), "Items")]
 public partial class History1 : ContentPage
 {
-    public List<HistoryItem> Items { get; set; }
+    public ObservableCollection<HistoryItem> Items = new ObservableCollection<HistoryItem>();
     public History1()
     {
         InitializeComponent();
-        ListViewItems.ItemsSource = Calculator.historyItems;
+        LoadData();
+
     }
 
-    private void ContentPage_NavigatedTo(object sender, NavigatedToEventArgs e)
+    private void LoadData()
     {
-        ListViewItems.ItemsSource = Calculator.historyItems;
+        Items.Clear();
+
+        Calculator
+            .historyItems
+            .ForEach(x =>
+                        Items.Add(x));
+        ListViewItems.ItemsSource = Items;
     }
+
+    protected async override void OnAppearing()
+    {
+        LoadData();
+        base.OnAppearing();
+        //collectionView.ItemsSource = await _dataService.GetAllToDosAsync();
+        //ListViewItems.ItemsSource = Calculator.historyItems;
+    }
+    //private void ContentPage_NavigatedTo(object sender, NavigatedToEventArgs e)
+    //{
+    //    LoadData();
+    //}
 }
--- src/Calculator/HistoryItem.cs
+++ src/Calculator/HistoryItem.cs
@@ -1,19 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Calculator
 {
-    public class HistoryItem
+    //public class HistoryItems : INotifyPropertyChanged
+    //{
+    //    private List<HistoryItem> _items;
+    //    public List<HistoryItem> Items
+    //    {
+    //        get => _items;
+    //        set
+    //        {
+    //            if (_items == value) return;
+    //            _items = value;
+    //            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Items)));
+    //        }
+    //    }
+    //    public HistoryItems()
+    //    {
+    //        Items = new List<HistoryItem>();
+    //    }
+    //    public event PropertyChangedEventHandler PropertyChanged;
+    //}
+    //public class HistoryItem
+    //{
+    //    public string QuestionAnswer { get; set; }
+    //    public string DateTime1 { get; set; }
+    //}
+    public class HistoryItem : INotifyPropertyChanged
     {
-        public string QuestionAnswer { get; set; }
-        public string DateTime1 { get; set; }
+        private string _q;
+        private string _dT;
+
+        public string QuestionAnswer
+        {
+            get => _q;
+            set
+            {
+                if (_q == value) return;
+                _q = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(QuestionAnswer)));
+            }
+        }
+        public string DateTime1
+        {
+            get => _dT; set
+            {
+                if (_dT == value) return;
+                _dT = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(DateTime1)));
+            }
+        }
 
         public HistoryItem()
         {
             DateTime1 = DateTime.Now.ToShortTimeString();
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }

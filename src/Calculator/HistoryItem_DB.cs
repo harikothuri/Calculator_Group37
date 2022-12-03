230f7bc0487f13d19a73e5b6a7d875f54e9cb2da
saikrishnachapalamadugu <saikrishnach2022@gmail.com>
2022-12-02 23:08:46 -0600

    Added new Entity

---
+++ src/Calculator/HistoryItem_DB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class HistoryItem_DB
+    {
+        public string QuestionAnswer { get; set; }
+        public string DateTime1 { get; set; }
+        public HistoryItem_DB()
+        {
+            DateTime1 = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+    }
+}
